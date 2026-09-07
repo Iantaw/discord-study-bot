8db469e1899076532b8e138e6d46c09c28b02bc6
Iantaw <ian@tawileh.net>
2026-09-07 16:15:52 -0400

    Created command stack (command files, handler, and deployment

---
+++ deploy-commands.js
@@ -0,0 +1,49 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { REST, Routes, Collection } from 'discord.js';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath, pathToFileURL } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const fileUrl = pathToFileURL(filePath).href;
+        const commandModule = await import(fileUrl);
+        const command = commandModule.default;
+
+        if ('data' in command && 'execute' in command) {
+            commands.set(command.data.name, command);
+        }
+    }
+}
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+const clientID = process.env.DISCORD_CLIENT_ID;
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.size} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationCommands(clientID),
+            { body: commands.map(c => c.data.toJSON()) }
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
--- index.js
+++ index.js
@@ -10,7 +10,14 @@ import {
     ModalBuilder,
     TextInputBuilder,
     TextInputStyle,
+    Collection,
+    Events,
+    MessageFlags
 } from 'discord.js';
+
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath, pathToFileURL } from 'node:url';
 
 const client = new Client({
     intents: [
@@ -20,7 +27,33 @@ const client = new Client({
     ],
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.commands = new Collection();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+(async () => {
+    for (const folder of commandFolders) {
+        const commandsPath = path.join(foldersPath, folder);
+        const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const fileUrl = pathToFileURL(filePath).href;
+            const commandModule = await import(fileUrl);
+            const command = commandModule.default;
+        
+            if ('data' in command && 'execute' in command) {
+                client.commands.set(command.data.name, command);
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
+        }
+    }
+
+    client.login(process.env.DISCORD_TOKEN);
+})();
 
 const btn = new ButtonBuilder()
         .setCustomId('btn')
@@ -28,27 +61,55 @@ const btn = new ButtonBuilder()
         .setStyle(ButtonStyle.Primary);
 const row = new ActionRowBuilder().addComponents(btn);
 
-client.on("messageCreate",  async (message) => {
+client.on("messageCreate", async (message) => {
     console.log(message);
 
     if (!message?.author.bot) {
-        message.author.send({
+        try {
+            await message.author.send({
+                content: 'Button test',
+                components: [row]
+            });
+        } catch {}
+        await message.channel.send({
             content: 'Button test',
             components: [row]
-        })
-        message.channel.send({
-            content: 'Button test',
-            components: [row]
-        })
+        });
     }
-
 });
 
 client.on('interactionCreate', async interaction => {
+    if (!interaction.isButton()) return;
     if (interaction.customId === 'btn') {
         await interaction.reply({
             content: 'Test',
             ephemeral: true
-        })
+        });
     }
-})
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return; 
+	const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: 'There was an error while executing this command!',
+                flags: MessageFlags.Ephemeral,
+            });
+        } else {
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                flags: MessageFlags.Ephemeral,
+            });
+        }
+    }
+});
